12cc5436bf74f4307bf44ce8a38f8583d956e4aa
chiefkufre <samuelkufrewillie@gmail.com>
2024-06-30 17:22:22 +0100

    chore: design the landing page for rasybanking with tailwindcss

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+const banner = document.getElementById('easy_mobile_banner')
+const banner_div = document.getElementById('banner_div')
+const heroImgOne = document.getElementById('hero_img_1')
+const heroImgTwo = document.getElementById('hero_img_2')
+const hero  = document.getElementById('hero')
+
+
+
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open')
   nav.classList.toggle('flex')
   nav.classList.toggle('hidden')
+  heroImgOne.classList.toggle('absolute')
+  heroImgTwo.classList.toggle('hidden')
+  nav.classList.add('z-10')
+  hero.classList.add('backdrop-opacity-60')
+  // banner.classList.toggle('hidden')
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colorMix = require('tailwindcss-color-mix');
+
 module.exports = {
   content: ['./*.html'],
   theme: {
@@ -18,9 +21,23 @@ module.exports = {
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
         veryLightGray: 'hsl(0, 0%, 98%)',
+
+        // EasyBanking Primary
+        darkBlueColor: "hsl(233, 26%, 24%)",
+        limeGreenColor: "hsl(136, 65%, 51%)",
+        brightCyanColor: "hsl(192, 70%, 51%)",
+
+        // EasyBanking Neutral
+        grayishBlueColor: "hsl(233, 8%, 62%)",
+        lightGrayishBlue: "hsl(220, 16%, 96%)",
+        veryLightGrayColor: "hsl(0, 0%, 98%)",
+        whiteColor: "hsl(0, 0%, 100%)",
+        mixed: 'color-mix(in hsl, var(--limeGreenColor)60%, var(--brightCyanColor)40%)'
       },
     },
   },
-  plugins: [],
+  plugins: [
+    colorMix(),
+  ],
 }
 
